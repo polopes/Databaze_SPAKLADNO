9b7d6614fcc173569703088308f4a49ae0dd8039
zedthe3rd <zciboch7@gmail.com>
2026-03-19 09:40:23 +0100

    ff

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, request
 import os
+import plotly.graph_objects as go
+import plotly.io as pio
 
 app = Flask(__name__) # nazev pred route
 
@@ -49,6 +51,55 @@ def nahrani_souboru():
     return render_template('index7.html', content=content)
 
 
+
+@app.route('/8')
+def graph():
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=[1, 2, 3, 4],
+        y=[10, 20, 25, 30],
+        mode='lines+markers',
+        name='Data 1'
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=[1, 2, 3, 4],
+        y=[15, 18, 22, 27],
+        mode='lines+markers',
+        name='Data 2'
+    ))
+
+    fig.update_layout(
+        title="Ukázkový interaktivní graf",
+        xaxis_title="X-osa",
+        yaxis_title="Y-osa",
+        template="plotly_white"
+    )
+
+    graph_html = pio.to_html(fig, full_html=False)
+
+    return render_template("index8.html", graph_html=graph_html)
+
+
+@app.route('/9/cint:id>/<string:name>',methods=['GET'])
+def parametry(id, name):
+    return render_template("index9.html", id=id, name=name)
+
+@app.route('/10', methods=['GET', 'POST'])
+def redirekting():
+    result = None
+    if request.method != 'POST':
+        return render_template("index10.html", result=result)
+    
+    number = request.form.get('number', type=int)
+    result = number
+    
+    if result == 1:
+        return redirect('/1')
+    elif result == 2:
+        return redirect('/2')
+    else:
+        return rende_teplate index 10.html ,result = result    
 if __name__ == '__main__':
     app.run()
-
